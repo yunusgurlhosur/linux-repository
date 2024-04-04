54cc1540fa0717ccf7328d0e75e0523b6f51cb67
yunus <yunusgurlhosur@gmail.com>
2024-04-04 03:57:55 +0000

    my commit

---
+++ node-health.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#########
+# author: Yunus
+# Date 4 April 2024
+# this script outputs the node health
+# VErsion: V1
+##########
+
+set -x # debug mode
+
+df -h
+
+
+
+free -g
+
+
+
+nproc
+
